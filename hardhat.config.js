42a42ce6beab542fe0f04bfb854e9cfa05789d6c
tangqiangn <tangqiangn@yonyou.com>
2024-12-16 11:16:47 +0800

    faet:fundme合约

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@chainlink/env-enc").config();
+// require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVE_KEY],
+      chainId: 11155111
+    }
+  },
+
+  // ETHERSCAN_API_KEY
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    }
+  }
 };
---
+++ scripts/deployFundMe.js
@@ -0,0 +1,36 @@
+// import ethers.js
+// create main function
+// execute main function
+
+const { ethers } = require("hardhat")
+
+async function main() {
+    // create factory
+    const fundMeFactory = await ethers.getContractFactory("FundMe")
+    console.log("contract deploying")
+    // deploy contract from factory
+    const fundMe = await fundMeFactory.deploy(300)
+    await fundMe.waitForDeployment()
+    console.log(`contract has been deployed successfully, contract address is ${fundMe.target}`);
+    if(hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for 5 confirmations")
+        await fundMe.deploymentTransaction().wait(5)
+        await verifyFundMe(fundMe.target, [300])
+    } else {
+        console.log("verification skipped..")
+    }
+
+
+}
+
+async function verifyFundMe(fundMeAddr, args) {
+    await hre.run("verify:verify", {
+        address: fundMeAddr,
+        constructorArguments: args,
+    });
+}
+
+main().then().catch((error) => {
+    console.error(error)
+    process.exit(0)
+})
